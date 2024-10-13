ba8f91d7a77d563c9762735829aea857ecc8dec5
Tanmoy Ganguly <xanmoy.tsx@gmail.com>
2024-10-13 23:04:27 +0530

    updated

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function index() {
+  return (
+      <View>
+          <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+export default index
