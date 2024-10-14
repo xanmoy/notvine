65e8a05b3002cc02198477269821d61b93f54f10
Tanmoy Ganguly <xanmoy.tsx@gmail.com>
2024-10-14 13:42:47 +0530

    fixed

---
+++ app/_layout.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot } from "expo-router";
+import "../global.css"
+
+
+export default function _layout() {
+  return (
+    <View>
+      <Slot />
+    </View>
+  )
+}
--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { StatusBar } from 'expo-status-bar';
 function index() {
   return (
-      <View>
-          <Text>Home Screen</Text>
-    </View>
+      
+      <View style={{paddingTop: 40}} className="bg-red-800">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    
   )
 }
 
